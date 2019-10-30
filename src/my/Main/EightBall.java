0c0c315d64f839e6599317f8992a50a0b9268eb2
Martin Huschenbett <martin-huschenbett@web.de>
2019-10-30 11:16:11 +0100

    init

---
+++ src/my/Main/EightBall.java
@@ -0,0 +1,23 @@
+package my.Main;
+
+import java.io.FileReader;
+
+
+public class EightBall {
+	public static void main(String args[]) throws Exception {
+		char[] buffer = new char[1024];
+		String filename = args[0];
+		try {
+			filename = "" + (Integer.parseInt(filename) % 3);
+		} catch (Exception e) {
+			System.out.println("Invalid input.");
+		}
+		if(filename.contains("..\\")) {
+			filename = filename.replace("..\\", "");
+		}
+		
+		FileReader fr = new FileReader(filename);
+		int out = fr.read(buffer);
+		System.out.println(buffer);		
+	}
+}

82134c7e13bc37ff704249f32dac0b164f92ad57
Martin Huschenbett <martin-huschenbett@web.de>
2019-10-30 11:28:20 +0100

    test commit

--- src/my/Main/EightBall.java
+++ src/my/Main/EightBall.java
@@ -18,6 +18,14 @@ public class EightBall {
 		
 		FileReader fr = new FileReader(filename);
 		int out = fr.read(buffer);
-		System.out.println(buffer);		
+		System.out.println(buffer);
+		System.out.println(buffer);	
+		System.out.println(buffer);	
+		System.out.println(buffer);	
+		System.out.println(buffer);	
+		System.out.println(buffer);	
+		System.out.println(buffer);	
+		System.out.println(buffer);
+		
 	}
 }
